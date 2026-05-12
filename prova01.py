0758596dd5a4712d99aad189e0d9ed207e02dc95
Jordi Lamarca <nowan15@hotmail.com>
2026-05-12 12:37:22 +0200

    Add files via upload

--- prova01.py
+++ prova01.py
@@ -1,4 +1,24 @@
 import streamlit as st
-
+import pandas as pd
+import matplotlib as plt
+import mysql.connector
+from mysql.connector import Error
 
 st.title("hello world")
+
+connection_config_dict = {
+        'user': 'root',
+        'password': 'root',
+        'host': '127.0.0.1',
+        'database': 'sprint4',
+        'raise_on_warnings': True,
+        'use_pure': False,
+        'autocommit': True,
+        'pool_size': 5
+    }
+connection = mysql.connector.connect(**connection_config_dict)
+
+
+pedidos = pd.read_sql('select * from users', connection )
+
+plt.plot(pedidos)
